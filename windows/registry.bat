ab6dc5bf7f8eb181ef46cda1d691f29d76132b96
Sharavanan Balasundaravel <sharavananpa@gmail.com>
2026-03-20 05:52:32 -0700

    Replace echo with rem when not required for easy debugging

--- windows/registry.bat
+++ windows/registry.bat
@@ -9,44 +9,44 @@ net file >nul 2>nul
 )
 @echo.
 
-@echo Revert Windows 11 context menu to classic style
+rem Revert Windows 11 context menu to classic style
 reg add "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /ve /d "" /f
 
-@echo Enable verbose status messages during shutdown/restart
+rem Enable verbose status messages during shutdown/restart
 reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v "verbosestatus" /t REG_DWORD /d 1 /f
 
-@echo Set File Explorer to open to "This PC" instead of "Quick Access"
+rem Set File Explorer to open to "This PC" instead of "Quick Access"
 reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "LaunchTo" /t REG_DWORD /d 1 /f
 
-@echo Disable "pretty path" display in address bar (show full paths)
+rem Disable "pretty path" display in address bar (show full paths)
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "DontPrettyPath" /t REG_DWORD /d 1 /f
 
-@echo Show hidden files and folders in File Explorer
+rem Show hidden files and folders in File Explorer
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "Hidden" /t REG_DWORD /d 1 /f
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "ShowSuperHidden" /t REG_DWORD /d 1 /f
 
-@echo Show file extensions for known file types
+rem Show file extensions for known file types
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "HideFileExt" /t REG_DWORD /d 0 /f
 
-@echo Set taskbar icon alignment to left (classic style)
+rem Set taskbar icon alignment to left (classic style)
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v "TaskbarAl" /t REG_DWORD /d 0 /f
 
-@echo Disable Windows Copilot AI assistant
+rem Disable Windows Copilot AI assistant
 reg add "HKCU\Software\Policies\Microsoft\Windows\WindowsCopilot" /v "TurnOffWindowsCopilot" /t REG_DWORD /d 1 /f
 
-@echo Disable Windows telemetry/data collection
+rem Disable Windows telemetry/data collection
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\DataCollection" /v "AllowTelemetry" /t REG_DWORD /d 0 /f
 
-@echo Disable automatic reboot after bluescreen
+rem Disable automatic reboot after bluescreen
 reg add "HKLM\System\CurrentControlSet\Control\CrashControl" /v "AutoReboot" /t REG_DWORD /d 0 /f
 
-@echo Give full error information in bluescreens
+rem Give full error information in bluescreens
 reg add "HKLM\System\CurrentControlSet\Control\CrashControl" /v "DisplayParameters" /t REG_DWORD /d 1 /f
 
-@echo Disable System Volume Information folder creation on removable media
+rem Disable System Volume Information folder creation on removable media
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows" /v "DisableRemovableDriveIndexing" /t REG_DWORD /d 1 /f
 
-@echo Remove bloat from Edge
+rem Remove bloat from Edge
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Edge" /v "EdgeEnhanceImagesEnabled" /t REG_DWORD /d 0 /f
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Edge" /v "PersonalizationReportingEnabled" /t REG_DWORD /d 0 /f
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Edge" /v "ShowRecommendationsEnabled" /t REG_DWORD /d 0 /f
